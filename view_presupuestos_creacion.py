906cf508e5aae24cb3bf3cd357298fc388dace8c
alvarogallegosz <alvarogallegosz@gmail.com>
2026-07-19 19:41:14 -0400

    Update view_presupuestos_creacion.py

--- view_presupuestos_creacion.py
+++ view_presupuestos_creacion.py
@@ -481,11 +481,11 @@ def render_creacion_presupuestos(rol_simulado):
                 <thead>
                     <tr>
                         <th style="width: 7%; text-align: center;">ITEM</th>
-                        <th style="width: 42%; text-align: left;">{sec_titulo}</th>
-                        <th style="width: 20%; text-align: left;">MEDIDAS</th>
-                        <th style="width: 10%; text-align: center;">JUEGOS/KITS</th>
-                        <th style="width: 10%; text-align: center;">CANT.</th>
-                        <th style="width: 10%; text-align: right;">PRECIO</th>
+                        <th style="width: 44%; text-align: left;">{sec_titulo}</th>
+                        <th style="width: 21%; text-align: left;">MEDIDAS</th>
+                        <th style="width: 9%; text-align: center;">JUEGOS/KITS</th>
+                        <th style="width: 8%; text-align: center;">CANT.</th>
+                        <th style="width: 11%; text-align: right;">PRECIO</th>
                     </tr>
                 </thead>
                 <tbody>
