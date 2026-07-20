09eefa7671a0a300b0e163739ff510ca8bafdab8
alvarogallegosz <alvarogallegosz@gmail.com>
2026-07-19 22:20:58 -0400

    Add optional parameter to render_creacion_presupuestos

--- view_presupuestos_creacion.py
+++ view_presupuestos_creacion.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import datetime
 
-def render_creacion_presupuestos():
+def render_creacion_presupuestos(rol_simulado=None):
     st.title("📝 Creación de Presupuesto")
     st.caption("Diligencie los campos y agregue los ítems requeridos para generar la propuesta.")
     
